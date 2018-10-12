f473a72943bacdc76ffbf1c44e17d04fb65ad22c
wyatt <wyattm94@users.noreply.github.com>
2018-10-12 17:11:25 -0400

    add css/function calls

--- app.py
+++ app.py
@@ -6,25 +6,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	# sp = yget_stock('^GSPC',start='2018-1-1')
-	# dj  = yget_stock('^DJI',start='2018-1-1')
-	# nd = yget_stock('^IXIC',start='2018-1-1')
+	sp = yget_stock('^GSPC',start='2018-1-1')
+	dj  = yget_stock('^DJI',start='2018-1-1')
+	nd = yget_stock('^IXIC',start='2018-1-1')
 	
-	# sp_p = yf_plot(sp)
-	# img1 = io.BytesIO(); plt.savefig(img1,format='png'); img1.seek(0)
-	# res_sp = base64.b64encode(img1.getvalue()).decode()
+	sp_p = yf_plot(sp)
+	img1 = io.BytesIO(); plt.savefig(img1,format='png'); img1.seek(0)
+	res_sp = base64.b64encode(img1.getvalue()).decode()
 	
-	# dj_p  = yf_plot(dj)
-	# img2 = io.BytesIO(); plt.savefig(img2,format='png'); img2.seek(0)
-	# res_dj = base64.b64encode(img2.getvalue()).decode()
+	dj_p  = yf_plot(dj)
+	img2 = io.BytesIO(); plt.savefig(img2,format='png'); img2.seek(0)
+	res_dj = base64.b64encode(img2.getvalue()).decode()
 	
-	# nd_p = yf_plot(nd)
-	# img3 = io.BytesIO(); plt.savefig(img3,format='png'); img3.seek(0)
-	# res_nd = base64.b64encode(img3.getvalue()).decode()
+	nd_p = yf_plot(nd)
+	img3 = io.BytesIO(); plt.savefig(img3,format='png'); img3.seek(0)
+	res_nd = base64.b64encode(img3.getvalue()).decode()
 	
-	# return render_template('home.html',fsp=res_sp,fdj=res_dj,fnd=res_nd)
-	
-	return render_template("home.html") #, title='Home Page') #posts=posts
+	return render_template('home.html',fsp=res_sp,fdj=res_dj,fnd=res_nd)
+	#return render_template("home.html") #, title='Home Page') #posts=posts
 
 @app.route('/page2')
 def page2():
